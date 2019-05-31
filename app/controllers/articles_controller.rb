4fb80d626904360f5e9f828a99cced198b3a46d5
Mihai Anca <mihai.anca@softvision.ro>
2019-05-31 10:23:39 +0000

    Style all the pages

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,61 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy] 
+   
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+      @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+   
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      #do something
+      flash[:success] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+          render 'new'
+    end
+  end
+  
+  def update 
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article was successfully updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+  
+  def show
+     @article = Article.find(params[:id]) 
+  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy  
+    # Article.destroy(params[:id])
+
+    flash[:danger] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+  
+  private
+  
+    def set_article
+      @article = Article.find(params[:id])  
+    end
+    
+    def article_params
+       params.require(:article).permit(:title, :description) 
+    end
+        
+    
+end
